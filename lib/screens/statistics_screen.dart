3251444ca67e536141edc335f2209ea77932b903
lucaszcai <lucas.z.cai@gmail.com>
2020-04-04 16:19:41 -0500

    working on stats page

--- lib/screens/statistics_screen.dart
+++ lib/screens/statistics_screen.dart
@@ -6,12 +6,37 @@ class StatisticsScreen extends StatefulWidget {
 }
 
 class _StatisticsScreenState extends State<StatisticsScreen> {
+
+  List<String> provinces = ['toronto', 'ottowa', 'katy', 'texas'];
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text('Statistics'),
-      ),
+      child: Column(
+        children: <Widget>[
+          Text(
+            'Statistics',
+            style: TextStyle(
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          ListView.builder(
+            itemCount: provinces.length,
+              itemBuilder: (context, index){
+                return Container(
+                  height: 100.0,
+                  child: Row(
+                    children: <Widget>[
+
+                    ],
+                  )
+                );
+              }
+          )
+        ],
+      )
     );
   }
 }
