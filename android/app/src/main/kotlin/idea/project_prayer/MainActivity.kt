59ce4c605828e967401e7d56d90e0d656abd0d2f
lucaszcai <lucas.z.cai@gmail.com>
2020-04-03 20:36:31 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/idea/project_prayer/MainActivity.kt
@@ -0,0 +1,12 @@
+package idea.project_prayer
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
