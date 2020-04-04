197252cb38dae1ffae00fcf0b8301bb006b75530
lucaszcai <lucas.z.cai@gmail.com>
2020-04-04 15:17:55 -0500

    Merge branch 'master' of https://github.com/lucaszcai/project_prayer
    # Conflicts:
    #	lib/screens/map_page.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -8,11 +8,86 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Container(
-          child: Text('profile'),
-        ),
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 50.0,
+              ),
+              Text(
+                'Vincent Do',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Avid Conservationalist',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 200,
+                child: Divider(
+                  color: Colors.orange.shade700,
+                ),
+              ),
+              Container(color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.flag,
+                      color: Colors.orange,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'Prayers: 25',
+                      style: TextStyle(
+                        color: Colors.orange.shade900,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.add_location,
+                      color: Colors.orange,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'Markers Created: 74',
+                      style: TextStyle(
+                        color: Colors.orange.shade900,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
       ),
     );
   }
