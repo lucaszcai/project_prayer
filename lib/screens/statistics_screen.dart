4ce5b019739befb8e5c3df9ef9ff1b4eebf46dde
Samuel Yuan <samuel6161@gmail.com>
2020-04-04 17:13:32 -0500

    stats page

--- lib/screens/statistics_screen.dart
+++ lib/screens/statistics_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class StatisticsScreen extends StatefulWidget {
   @override
@@ -6,12 +10,76 @@ class StatisticsScreen extends StatefulWidget {
 }
 
 class _StatisticsScreenState extends State<StatisticsScreen> {
+  List<Case> provinceCases;
+
+  @override
+  void initState() {
+    super.initState();
+    provinceCases = new List<Case>();
+    fillCases();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Statistics'),
-      ),
-    );
+    if (provinceCases.length == 0) {
+      return Scaffold(
+        body: CircularProgressIndicator(),
+      );
+    } else {
+      provinceCases.sort((a, b) => b.cases.compareTo(a.cases));
+      return Scaffold(
+        body: Column(
+          children: <Widget>[
+            SizedBox(height: MediaQuery.of(context).size.height / 20,),
+            Text('Current Statistics', textAlign: TextAlign.center, style: TextStyle(fontSize: 20),),
+            SizedBox(height: MediaQuery.of(context).size.height / 30,),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: provinceCases.length,
+                  itemBuilder: (context, index) {
+                    return (Column(
+                      children: <Widget>[
+                        ListTile(
+                          title: Text(provinceCases[index].province),
+                          subtitle: Text(
+                              'Infected: ${provinceCases[index].cases.toString()}'),
+                          leading: Text('#${index + 1}'),
+                        ),
+                        Divider()
+                      ],
+                    ));
+                  }),
+            )
+          ],
+        )
+      );
+    }
   }
+
+  getPlacemark() async {
+    List<Placemark> list =
+        await Geolocator().placemarkFromCoordinates(56.13, -106.35);
+    print(list[0].administrativeArea);
+  }
+
+  fillCases() async {
+    print('start');
+    var value = await http.get(
+        "https://api.apify.com/v2/key-value-stores/fabbocwKrtxSDf96h/records/LATEST?disableRedirect=true");
+    var allData = json.decode(value.body)['infectedByRegion'];
+    print("LENGTH: " + allData.length.toString());
+    for (int i = 1; i < allData.length - 1; i++) {
+      Case current = new Case(
+          allData[i]['region'], int.parse(allData[i]['infectedCount']));
+      provinceCases.add(current);
+    }
+    setState(() {});
+  }
+}
+
+class Case {
+  String province;
+  int cases;
+
+  Case(this.province, this.cases);
 }
